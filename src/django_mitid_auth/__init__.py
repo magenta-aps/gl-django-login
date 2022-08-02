b30e13a8b0631f4298eec1db536b97e05b1ccc8f
Lars Peter Thomsen <larsp@magenta.dk>
2022-08-02 17:34:00 +0200

    Debugging

--- src/django_mitid_auth/__init__.py
+++ src/django_mitid_auth/__init__.py
@@ -2,5 +2,5 @@ from django.conf import settings
 from django.utils.module_loading import import_string
 
 
-def loginprovider():
+def login_provider_class():
     return import_string(settings.LOGIN_PROVIDER_CLASS or 'django_mitid_auth.dummy.dummy.DummyProvider')
