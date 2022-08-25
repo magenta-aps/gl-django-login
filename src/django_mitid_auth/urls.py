87be264b8a48c2ddfe0a7934ad73fba3d05fc51f
Lars Peter Thomsen <larsp@magenta.dk>
2022-08-25 10:49:12 +0200

    Make urls optional

--- src/django_mitid_auth/urls.py
+++ src/django_mitid_auth/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.conf import settings
 
 from django_mitid_auth.apps import LoginConfig
 from django_mitid_auth.views import LoginView, LoginCallbackView, LogoutView, LogoutCallback
@@ -10,6 +11,12 @@ urlpatterns = [
     path('login/callback/', LoginCallbackView.as_view(), name='login-callback'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('logout/callback/', LogoutCallback.as_view(), name='logout-callback'),
-    path('oid/', include('django_mitid_auth.openid.urls', namespace='oid')),
-    path('saml/', include('django_mitid_auth.saml.urls', namespace='saml')),
 ]
+if settings.LOGIN_PROVIDER_CLASS == 'django_mitid_auth.openid.openid.OpenId':
+    urlpatterns += [
+        path('oid/', include('django_mitid_auth.openid.urls', namespace='oid')),
+    ]
+if settings.LOGIN_PROVIDER_CLASS in ('django_mitid_auth.saml.saml2.Saml2', 'django_mitid_auth.saml.oiosaml.OIOSaml'):
+    urlpatterns += [
+        path('saml/', include('django_mitid_auth.saml.urls', namespace='saml')),
+    ]
