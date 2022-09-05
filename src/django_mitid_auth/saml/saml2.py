62c256506cf448f143ebe75cf23b2e69fe994636
Lars Peter Thomsen <larsp@magenta.dk>
2022-09-05 08:25:12 +0200

    Update metadata generation

--- src/django_mitid_auth/saml/saml2.py
+++ src/django_mitid_auth/saml/saml2.py
@@ -308,7 +308,7 @@ class Saml2(LoginProvider):
                 dig = DigestMethod()
                 dig.algorithm = "http://www.w3.org/2000/09/xmldsig#sha1"
                 enc2.digest_method = [dig]
-                key_descriptor.encryption_method = [enc2]
+                key_descriptor.encryption_method = [enc1, enc2]
 
     """
     @classmethod
