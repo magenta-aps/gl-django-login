3fba5d67a7822ac1ed617947d1635d30c1244eab
Lars Peter Thomsen <larsp@magenta.dk>
2022-09-14 13:57:36 +0200

    testing

---
+++ src/django_mitid_auth/management/commands/update_mitid_idp_metadata.py
@@ -0,0 +1,27 @@
+import os.path
+
+import requests
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Wait for database to be available"
+
+    def get_url(self):
+        return settings.SAML.get("metadata_remote")
+
+    def get_cache_filename(self):
+        if "local" in settings.SAML["metadata"] and len(settings.SAML["metadata"]["local"]):
+            return settings.SAML["metadata"]["local"][0]
+
+    def handle(self, *args, **options):
+        remote_url = self.get_url()
+        filename = self.get_cache_filename()
+        if remote_url and filename:
+            with open(filename, "wb") as file:
+                response = requests.get(remote_url)
+                if response.status_code == 200:
+                    file.write(response.content)
+        if filename and not os.path.exists(filename):
+            raise Exception("IdP Metadata file doesn't exist")
