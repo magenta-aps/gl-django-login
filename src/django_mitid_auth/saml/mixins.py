daebfcee7cfe48d1edcf154662528bdbf06ec7fa
Lars Peter Thomsen <larsp@magenta.dk>
2022-09-14 10:27:15 +0200

    Customizable permission denied

--- src/django_mitid_auth/saml/mixins.py
+++ src/django_mitid_auth/saml/mixins.py
@@ -9,12 +9,15 @@ class MitIdLOAMixin:
     LEVEL_SUBSTANTIAL = 'Substantial'
     LEVEL_HIGH = 'High'
     required_level_of_assurance = LEVEL_LOW
+    error_template = "django_mitid_auth/insufficient_permissions.html"
 
     def dispatch(self, request, *args, **kwargs):
         if OIOSaml.enabled():
             user_level_of_assurance = request.session['user_info'].get('LevelOfAssurance')
-            if user_level_of_assurance is None:
-                raise PermissionDenied
-            if self.levels.index(user_level_of_assurance) < self.levels.index(self.required_level_of_assurance):
-                raise PermissionDenied
+            if user_level_of_assurance is None \
+                    or self.levels.index(user_level_of_assurance) < self.levels.index(self.required_level_of_assurance):
+                return self.permission_denied(request, *args, **kwargs)
         return super().dispatch(request, *args, **kwargs)
+
+    def permission_denied(self, request, *args, **kwargs):
+        raise PermissionDenied
