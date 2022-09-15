dd1e8ab4769898f70f22e363771c8d4b5493b6aa
Lars Peter Thomsen <larsp@magenta.dk>
2022-09-15 20:52:44 +0200

    Refactor

--- src/django_mitid_auth/saml/urls.py
+++ src/django_mitid_auth/saml/urls.py
@@ -1,8 +1,6 @@
 from django.conf import settings
 from django.urls import path, reverse_lazy
-from django.views.generic import TemplateView
-
-from django_mitid_auth.saml.views import MetadataView
+from django_mitid_auth.saml.views import MetadataView, AccessDeniedView
 
 app_name = 'django_mitid_auth.saml'
 
@@ -11,17 +9,17 @@ urlpatterns = [
 
     path(
         "error/login-timeout/",
-        TemplateView.as_view(template_name="django_mitid_auth/login_timeout.html", extra_context={'login_url': reverse_lazy(f"{settings.LOGIN_NAMESPACE}:login")}),
+        AccessDeniedView.as_view(template_name="django_mitid_auth/login_timeout.html", extra_context={'login_url': reverse_lazy(f"{settings.LOGIN_NAMESPACE}:login")}),
         name="login-timeout",
     ),
     path(
         "error/login-repeat/",
-        TemplateView.as_view(template_name="django_mitid_auth/login_repeat.html", extra_context={'login_url': reverse_lazy(f"{settings.LOGIN_NAMESPACE}:login")}),
+        AccessDeniedView.as_view(template_name="django_mitid_auth/login_repeat.html", extra_context={'login_url': reverse_lazy(f"{settings.LOGIN_NAMESPACE}:login")}),
         name="login-repeat",
     ),
     path(
         "error/login-nocprcvr/",
-        TemplateView.as_view(template_name="django_mitid_auth/login_no_cprcvr.html", extra_context={'logout_url': reverse_lazy(f"{settings.LOGIN_NAMESPACE}:logout")}),
+        AccessDeniedView.as_view(template_name="django_mitid_auth/login_no_cprcvr.html", extra_context={'logout_url': reverse_lazy(f"{settings.LOGIN_NAMESPACE}:logout")}),
         name="login-no-cprcvr",
     ),
 ]
