3d5f60139d3af02d092f3637e0831931aa6d0b93
Lars Peter Thomsen <larsp@magenta.dk>
2023-11-22 16:06:18 +0100

    Debug

--- src/django_mitid_auth/saml/saml2.py
+++ src/django_mitid_auth/saml/saml2.py
@@ -119,7 +119,6 @@ class Saml2(LoginProvider):
 
     @classmethod
     def handle_login_callback(cls, request, success_url):
-        print(f"saml2.handle_login_callback({success_url})")
         """Handle an AuthenticationResponse from the IdP."""
         client = cls.get_client()
 
