f4a8eb057e0e2393a2844adc2b5c55059bc43c14
Lars Peter Thomsen <larsp@magenta.dk>
2023-11-23 09:52:53 +0100

    Debug

--- src/django_mitid_auth/saml/saml2.py
+++ src/django_mitid_auth/saml/saml2.py
@@ -132,6 +132,8 @@ class Saml2(LoginProvider):
             authn_response = client.parse_authn_request_response(
                 samlresponse, "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
             )
+            print(f'caches["saml"]: {caches["saml"]}')
+            print(f"authn_response.in_response_to: {authn_response.in_response_to}")
             if caches["saml"].get("message_id__" + authn_response.in_response_to):
                 caches["saml"].set("message_id__" + authn_response.in_response_to, None)
             else:
