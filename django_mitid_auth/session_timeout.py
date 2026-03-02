98b64f1edff03123cb5a1fef25fecf5e47039257
Lars Peter Thomsen <larsp@magenta.dk>
2026-03-02 15:33:15 +0100

    feat: [#67722] Add session timeout middleware

--- django_mitid_auth/session_timeout.py
+++ django_mitid_auth/session_timeout.py
@@ -7,13 +7,33 @@ from django.utils.module_loading import import_string
 
 """
 The following code is forked from https://github.com/labd/django-session-timeout
+We add functionality to allow a callable to be specified in settings.SESSION_EXPIRE_CALLABLE,
+that is called when the session expires.
+The callable is specified as a string reference to a function
+"""
+
+"""
+The MIT License (MIT)
 
 Copyright (c) 2017 Michael van Tellingen
 
-MIT License
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-We add functionality to allow a callable to be specified in settings.SESSION_EXPIRE_CALLABLE, that is called when the session expires.
-The callable is specified as a string reference to a function
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 SESSION_TIMEOUT_KEY = "_session_init_timestamp_"
 
