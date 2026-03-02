408bbd5a7fdd5b043720ba3e3d2af5a9aa90aaaf
Lars Peter Thomsen <larsp@magenta.dk>
2026-03-02 14:24:27 +0100

    feat: [#67722] Add session timeout middleware

--- django_mitid_auth/middleware.py
+++ django_mitid_auth/middleware.py
@@ -1,16 +1,13 @@
 import re
-import time
 from typing import List
 from urllib.parse import quote_plus
 
 from django.conf import settings
-from django.contrib.auth.views import redirect_to_login
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect
 from django.template.loader import get_template
 from django.urls import reverse
 from django.utils.http import urlencode
-from django.utils.module_loading import import_string
 
 from django_mitid_auth import login_provider_class
 
@@ -137,51 +134,3 @@ class LoginManager:
     def get_whitelisted_urls():
         return LoginManager(None).white_listed_urls
 
-
-"""
-The following code is forked from https://github.com/labd/django-session-timeout
-MIT License
-
-We add functionality to allow a callable to be specified in settings.SESSION_EXPIRE_CALLABLE, that is called when the session expires.
-The callable is specified as a string reference to a function
-"""
-SESSION_TIMEOUT_KEY = "_session_init_timestamp_"
-
-
-class SessionTimeoutMiddleware:
-    def process_request(self, request):
-        if not hasattr(request, "session") or request.session.is_empty():
-            return
-
-        init_time = request.session.setdefault(SESSION_TIMEOUT_KEY, time.time())
-
-        expire_seconds = getattr(
-            settings, "SESSION_EXPIRE_SECONDS", settings.SESSION_COOKIE_AGE
-        )
-
-        session_is_expired = time.time() - init_time > expire_seconds
-
-        if session_is_expired:
-            request.session.flush()
-            # Begin additional functionality
-            expire_callable = getattr(settings, "SESSION_EXPIRE_CALLABLE", None)
-            if type(expire_callable) == str:
-                expire_callable = import_string(expire_callable)
-                if callable(expire_callable):
-                    return expire_callable(request)
-            # End additional functionality
-            redirect_url = getattr(settings, "SESSION_TIMEOUT_REDIRECT", None)
-            if redirect_url:
-                return redirect(redirect_url)
-            else:
-                return redirect_to_login(next=request.path)
-
-        expire_since_last_activity = getattr(
-            settings, "SESSION_EXPIRE_AFTER_LAST_ACTIVITY", False
-        )
-        grace_period = getattr(
-            settings, "SESSION_EXPIRE_AFTER_LAST_ACTIVITY_GRACE_PERIOD", 1
-        )
-
-        if expire_since_last_activity and time.time() - init_time > grace_period:
-            request.session[SESSION_TIMEOUT_KEY] = time.time()
---
+++ django_mitid_auth/session_timeout.py
@@ -0,0 +1,54 @@
+import time
+
+from django.conf import settings
+from django.contrib.auth.views import redirect_to_login
+from django.shortcuts import redirect
+from django.utils.module_loading import import_string
+
+"""
+The following code is forked from https://github.com/labd/django-session-timeout
+MIT License
+
+We add functionality to allow a callable to be specified in settings.SESSION_EXPIRE_CALLABLE, that is called when the session expires.
+The callable is specified as a string reference to a function
+"""
+SESSION_TIMEOUT_KEY = "_session_init_timestamp_"
+
+
+class SessionTimeoutMiddleware:
+    def process_request(self, request):
+        if not hasattr(request, "session") or request.session.is_empty():
+            return
+
+        init_time = request.session.setdefault(SESSION_TIMEOUT_KEY, time.time())
+
+        expire_seconds = getattr(
+            settings, "SESSION_EXPIRE_SECONDS", settings.SESSION_COOKIE_AGE
+        )
+
+        session_is_expired = time.time() - init_time > expire_seconds
+
+        if session_is_expired:
+            request.session.flush()
+            # Begin additional functionality
+            expire_callable = getattr(settings, "SESSION_EXPIRE_CALLABLE", None)
+            if type(expire_callable) is str:
+                expire_callable = import_string(expire_callable)
+                if callable(expire_callable):
+                    return expire_callable(request)
+            # End additional functionality
+            redirect_url = getattr(settings, "SESSION_TIMEOUT_REDIRECT", None)
+            if redirect_url:
+                return redirect(redirect_url)
+            else:
+                return redirect_to_login(next=request.path)
+
+        expire_since_last_activity = getattr(
+            settings, "SESSION_EXPIRE_AFTER_LAST_ACTIVITY", False
+        )
+        grace_period = getattr(
+            settings, "SESSION_EXPIRE_AFTER_LAST_ACTIVITY_GRACE_PERIOD", 1
+        )
+
+        if expire_since_last_activity and time.time() - init_time > grace_period:
+            request.session[SESSION_TIMEOUT_KEY] = time.time()
